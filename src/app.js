f9de306cbbd10636c74d44f8944b0c9187791ee6
Cc Zheng <zchan0@outlook.com>
2018-01-23 23:56:49 +0800

    load google map

---
+++ src/app.js
@@ -0,0 +1,13 @@
+import './style.css'
+
+let map;
+
+function initMap() {
+    map = new google.maps.Map(document.getElementById('map'), {
+        center: {lat: -34.397, lng: 150.644},
+        zoom: 8
+    });
+}
+
+initMap();
+
